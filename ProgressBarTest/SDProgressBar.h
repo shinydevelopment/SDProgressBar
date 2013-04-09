b085bb8415f1fd930ef37bc3ea5e6d06204f9667
Dave Verwer <dave.verwer@shinydevelopment.com>
2013-04-09 15:34:21 +0100

    Added indeterminate status to the progress bar.

--- ProgressBarTest/SDProgressBar.h
+++ ProgressBarTest/SDProgressBar.h
@@ -1,6 +1,7 @@
 @interface SDProgressBar : UIView
 
 @property (assign, nonatomic) CGFloat progress;
+@property (assign, nonatomic) BOOL indeterminate;
 @property (strong, nonatomic) UIColor *barColor;
 @property (strong, nonatomic) UIColor *outlineColor;
 
--- ProgressBarTest/SDProgressBar.m
+++ ProgressBarTest/SDProgressBar.m
@@ -2,6 +2,7 @@
 
 @interface SDProgressBar ()
 @property (strong, nonatomic) CALayer *barLayer;
+@property (strong, nonatomic) CALayer *indeterminateLayer;
 @end
 
 @implementation SDProgressBar
@@ -24,9 +25,13 @@
 #pragma mark Private methods
 - (void)configureView
 {
+  // Defaults for the bar and outline colours
+  self.barColor = [UIColor whiteColor];
+  self.outlineColor = [UIColor whiteColor];
+
   // Create and set the fixed properties of the bar layer used to show the progress
   self.barLayer = [CALayer layer];
-  self.barLayer.backgroundColor = [UIColor whiteColor].CGColor;
+  self.barLayer.backgroundColor = self.barColor.CGColor;
   [self.layer addSublayer:self.barLayer];
 
   // Add the bar mask ready to be used to reveal the progress
@@ -37,12 +42,22 @@
 
   // Configure the outline on the main layer
   self.layer.backgroundColor = [UIColor clearColor].CGColor;
-  self.layer.borderColor = [UIColor whiteColor].CGColor;
+  self.layer.borderColor = self.outlineColor.CGColor;
+
+  // Create the indeterminate layer and hide it by default
+  self.indeterminateLayer = [CALayer layer];
+  self.indeterminateLayer.masksToBounds = YES;
+  self.indeterminateLayer.opacity = 0;
+  [self.layer addSublayer:self.indeterminateLayer];
+
+  // Add a layer ready to have the striped indeterminate background inserted into it
+  [self.indeterminateLayer addSublayer:[CALayer layer]];
 
   // KVO the progress and colour properties
   [self addObserver:self forKeyPath:@"progress" options:NSKeyValueObservingOptionNew context:nil];
   [self addObserver:self forKeyPath:@"barColor" options:NSKeyValueObservingOptionNew context:nil];
   [self addObserver:self forKeyPath:@"outlineColor" options:NSKeyValueObservingOptionNew context:nil];
+  [self addObserver:self forKeyPath:@"indeterminate" options:NSKeyValueObservingOptionNew context:nil];
 }
 
 #pragma mark Cleanup
@@ -51,6 +66,7 @@
   [self removeObserver:self forKeyPath:@"progress"];
   [self removeObserver:self forKeyPath:@"barColor"];
   [self removeObserver:self forKeyPath:@"outlineColor"];
+  [self removeObserver:self forKeyPath:@"indeterminate"];
 }
 
 #pragma mark KVO
@@ -68,9 +84,15 @@
   } else if ([keyPath isEqualToString:@"barColor"]) {
     UIColor *color = [change valueForKey:NSKeyValueChangeNewKey];
     self.barLayer.backgroundColor = color.CGColor;
+    CALayer *stripeLayer = [self.indeterminateLayer.sublayers lastObject];
+    stripeLayer.backgroundColor = [self indeterminatePatternColor].CGColor;
   } else if ([keyPath isEqualToString:@"outlineColor"]) {
     UIColor *color = [change valueForKey:NSKeyValueChangeNewKey];
     self.layer.borderColor = color.CGColor;
+  } else if ([keyPath isEqualToString:@"indeterminate"]) {
+    BOOL isIndeterminate = [[change valueForKey:NSKeyValueChangeNewKey] boolValue];
+    self.barLayer.opacity = isIndeterminate ? 0 : 1;
+    self.indeterminateLayer.opacity = isIndeterminate ? 1 : 0;
   }
 }
 
@@ -102,9 +124,94 @@
   self.barLayer.position = CGPointMake(viewWidth / 2, viewHeight / 2);
   self.barLayer.cornerRadius = scaledHeight / 2;
 
+  // Position the indeterminate layer over the top of the regular progress bar layer
+  self.indeterminateLayer.bounds = self.barLayer.bounds;
+  self.indeterminateLayer.position = self.barLayer.position;
+  self.indeterminateLayer.cornerRadius = self.barLayer.cornerRadius;
+
+  // Configure the stripe layer to be slightly wider and have an image generated from the height of the indeterminate bar
+  CALayer *stripeLayer = [self.indeterminateLayer.sublayers lastObject]; // Only one layer in the indeterminate layer
+  stripeLayer.bounds = CGRectMake(0, 0, self.indeterminateLayer.bounds.size.width + self.indeterminateLayer.bounds.size.height, self.indeterminateLayer.bounds.size.height);
+  stripeLayer.position = CGPointMake((self.indeterminateLayer.bounds.size.width / 2) - (self.indeterminateLayer.bounds.size.height / 2), self.indeterminateLayer.bounds.size.height / 2);
+  stripeLayer.backgroundColor = [self indeterminatePatternColor].CGColor;
+
+  // Restart the animation as the dimensions of the image may just have changed
+  [stripeLayer removeAllAnimations];
+  [stripeLayer addAnimation:[self animationForIndeterminateLayer:self.indeterminateLayer] forKey:nil];
+
   // Configure the mask ready for animating the reveal of the bar layer
   self.barLayer.mask.position = CGPointMake(0, scaledHeight / 2);
   self.barLayer.mask.bounds = CGRectMake(0, 0, self.progress * scaledWidth, scaledHeight);
+}
+
+#pragma mark Indeterminate image helpers
+- (UIColor *)indeterminatePatternColor
+{
+  CGFloat patternHeight = self.indeterminateLayer.bounds.size.height;
+  UIImage *patternImage = [self imageForIndeterminateBarWithHeight:patternHeight tintedWithColor:self.barColor];
+  return [UIColor colorWithPatternImage:patternImage];
+}
+
+- (UIImage *)imageForIndeterminateBarWithHeight:(CGFloat)height tintedWithColor:(UIColor *)color;
+{
+  // Make a square image the size of the height of the indeterminate bar
+  CGRect imageRect = CGRectMake(0, 0, height, height);
+  UIGraphicsBeginImageContextWithOptions(imageRect.size, YES, 0);
+  CGContextRef context = UIGraphicsGetCurrentContext();
+  CGContextTranslateCTM(context, 0, imageRect.size.height);
+  CGContextScaleCTM(context, 1, -1);
+
+  // Fill the whole image with the bar colour
+  CGContextSetFillColorWithColor(context, color.CGColor);
+  CGContextFillRect(context, CGRectInset(imageRect, -1, -1)); // Expanded 1pt to prevent edge gaps when tiling
+
+  // Create the path of a parallelogram to become one stripe of many
+  CGMutablePathRef path = CGPathCreateMutable();
+  CGPathMoveToPoint(path, nil, 0, 0);                                                       /*  __________  */
+  CGPathAddLineToPoint(path, nil, imageRect.size.width / 2, imageRect.size.height);         /*  |\    \  |  */
+  CGPathAddLineToPoint(path, nil, imageRect.size.width, imageRect.size.height);             /*  | \    \ |  */
+  CGPathAddLineToPoint(path, nil, imageRect.size.width / 2, 0);                             /*  |__\____\|  */
+  CGPathCloseSubpath(path);
+  CGContextAddPath(context, path);
+
+  // Should the tinted area darken or lighten the primary bar colour?
+  CGFloat whiteValue = round(1 - [self colorBrightnessForColor:color]);
+  CGContextSetFillColorWithColor(context, [UIColor colorWithWhite:whiteValue alpha:0.2].CGColor);
+  CGContextFillPath(context);
+  CGPathRelease(path);
+
+  // Grab the image and clean up
+  UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+  UIGraphicsEndImageContext();
+  return image;
+}
+
+- (CABasicAnimation *)animationForIndeterminateLayer:(CALayer *)layer
+{
+  const CGFloat animationTimeToHeightScaleFactor = 60; // Higher numbers for faster animation
+  CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position.x"];
+  animation.duration = layer.bounds.size.height / animationTimeToHeightScaleFactor;
+  animation.byValue = @(layer.bounds.size.height);
+  animation.repeatCount = HUGE_VALF;
+  return animation;
+}
+
+- (CGFloat)colorBrightnessForColor:(UIColor *)color
+{
+	NSInteger numberOfColorComponents = CGColorGetNumberOfComponents([color CGColor]);
+  if (numberOfColorComponents == 2) { // Grayscale
+    CGFloat white = 0;
+    [color getWhite:&white alpha:nil];
+    return white;
+  } else if (numberOfColorComponents == 4) { // RGB compatible
+    CGFloat red = 0, green = 0, blue = 0;
+    [color getRed:&red green:&green blue:&blue alpha:nil];
+    // Using algorithm from http://www.w3.org/WAI/ER/WD-AERT/#color-contrast
+    return ((red * 299) + (green * 587) + (blue * 114)) / 1000;
+  } else {
+    NSAssert(NO, @"Unexpected number of components in colour space");
+    return -1;
+  }
 }
 
 @end
